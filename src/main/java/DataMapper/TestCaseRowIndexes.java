b76528c96baa14009aa9d0bf50c402488a2d2eea
kcguttikonda <kcguttikonda@users.noreply.github.com>
2018-08-30 20:37:14 +0530

    -Implemented Config.yaml and its class

--- src/main/java/DataMapper/TestCaseRowIndexes.java
+++ src/main/java/DataMapper/TestCaseRowIndexes.java
@@ -1,6 +1,7 @@
 package DataMapper;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.testng.annotations.Test;
 
@@ -14,38 +15,49 @@ import java.util.Map;
 import static DataMapper.ConfigBuilder.TestCases;
 
 public class TestCaseRowIndexes {
-    static List rowValueList =new ArrayList();
+    static List rowValueList = new ArrayList();
 
     static Map<String, List> testCaseRows = new HashMap<String, List>();
     static Sheet testcasesData;
     static ExcelReader reader = new ExcelReader();
+    static int i = 0;
+    static int currentKey,currentTestCaseValue;
     public static void main(String args[]) throws IOException, InvalidFormatException {
         //setRowIndexes(){
         ConfigBuilder.configReader();
         System.out.println(ConfigBuilder.TestCasesPath);
         testcasesData = reader.TestCaseReader(ConfigBuilder.TestCasesPath);
 
+        System.out.println(ConfigBuilder.TestCases.size());
+        while(i<ConfigBuilder.TestCases.size()) {
+            for(int j=1;j<=ExcelReader.sheet.getLastRowNum();j++) {
 
-        for(int i=1;i<ExcelReader.sheet.getLastRowNum();i++) {
-            for(int j=0;j<=ConfigBuilder.TestCases.size();j++){
-               //System.out.println("im in 2nd loop");
-               //System.out.println(TestCases.get(i));
-               //System.out.println(ExcelReader.sheet.getRow(j).getCell(0));
-               //System.out.println(ExcelReader.sheet.getRow(j).getCell(1));
-               if(testcasesData.getRow(i)==null){
-                   System.out.println("im in if");
-                   continue;
-               }
+                if(testcasesData.getRow(j) == null){
+                    //System.out.println("im in if");
+                    continue;
+                }
 
-               else{
-                   if (ExcelReader.sheet.getRow(j).getCell(0).toString()==TestCases.get(i)){
-                       System.out.println("im in else");
-                       rowValueList.add(ExcelReader.sheet.getRow(j).getCell(0).getRowIndex());
-                   }
-                   }
-           }
-       }
-       System.out.println(testCaseRows);
-       System.out.println(rowValueList);
+                else{
+                    currentKey = Integer.parseInt(TestCases.get(i).toString());
+                    currentTestCaseValue = (int) Double.parseDouble(ExcelReader.sheet.getRow(j).getCell(0).toString());
+                    //System.out.println(currentTestCaseValue);
+                    //System.out.println(ExcelReader.sheet.getRow(j).getCell(1));
+                    //System.out.println((ExcelReader.sheet.getRow(j).getCell(0).getNumericCellValue()));
+                    //System.out.println(TestCases.get(i));
+
+                    if (currentTestCaseValue == currentKey){
+                        //System.out.println("im in else");
+                        System.out.println(ExcelReader.sheet.getRow(j).getCell(1).toString());
+                        rowValueList.add(ExcelReader.sheet.getRow(j).getCell(0).getRowIndex());
+                        testCaseRows.put((TestCases.get(i).toString()),rowValueList);
+                    }
+                }
+
+            }
+
+            i++;
+
+        }
+        System.out.println(testCaseRows);
     }
 }
