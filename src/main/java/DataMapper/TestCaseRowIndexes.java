e53905ebd74c393bc32379910ee7b56f56031570
kcguttikonda <kcguttikonda@users.noreply.github.com>
2018-08-30 21:52:44 +0530

    -Implemented TestCaseRowIndexesClass which enables framework to run the test cases based upon the entries from config.yml

--- src/main/java/DataMapper/TestCaseRowIndexes.java
+++ src/main/java/DataMapper/TestCaseRowIndexes.java
@@ -1,11 +1,8 @@
 package DataMapper;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.testng.annotations.Test;
-
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -13,50 +10,29 @@ import java.util.List;
 import java.util.Map;
 
 import static DataMapper.ConfigBuilder.TestCases;
-
 public class TestCaseRowIndexes {
-    static List rowValueList = new ArrayList();
-
-    static Map<String, List> testCaseRows = new HashMap<String, List>();
-    static Sheet testcasesData;
     static ExcelReader reader = new ExcelReader();
     static int i = 0;
     static int currentKey,currentTestCaseValue;
-    public static void main(String args[]) throws IOException, InvalidFormatException {
-        //setRowIndexes(){
-        ConfigBuilder.configReader();
-        System.out.println(ConfigBuilder.TestCasesPath);
-        testcasesData = reader.TestCaseReader(ConfigBuilder.TestCasesPath);
+    public static Map<String, List> testCaseRows = new HashMap<String, List>();
+    public void buildTestCasesList(List TestCases,Sheet testcasesData) throws IOException, InvalidFormatException {
 
-        System.out.println(ConfigBuilder.TestCases.size());
-        while(i<ConfigBuilder.TestCases.size()) {
+        while(i<TestCases.size()) {
+            List rowValueList = new ArrayList();
             for(int j=1;j<=ExcelReader.sheet.getLastRowNum();j++) {
-
                 if(testcasesData.getRow(j) == null){
                     //System.out.println("im in if");
                     continue;
                 }
-
                 else{
                     currentKey = Integer.parseInt(TestCases.get(i).toString());
                     currentTestCaseValue = (int) Double.parseDouble(ExcelReader.sheet.getRow(j).getCell(0).toString());
-                    //System.out.println(currentTestCaseValue);
-                    //System.out.println(ExcelReader.sheet.getRow(j).getCell(1));
-                    //System.out.println((ExcelReader.sheet.getRow(j).getCell(0).getNumericCellValue()));
-                    //System.out.println(TestCases.get(i));
-
                     if (currentTestCaseValue == currentKey){
-                        //System.out.println("im in else");
-                        System.out.println(ExcelReader.sheet.getRow(j).getCell(1).toString());
                         rowValueList.add(ExcelReader.sheet.getRow(j).getCell(0).getRowIndex());
                         testCaseRows.put((TestCases.get(i).toString()),rowValueList);
                     }
                 }
-
-            }
-
-            i++;
-
+            }i++;
         }
         System.out.println(testCaseRows);
     }
--- src/main/java/coreEngine/automationEngine.java
+++ src/main/java/coreEngine/automationEngine.java
@@ -1,35 +1,48 @@
 package coreEngine;
 import Actions.keywordActions;
 import DataMapper.ConfigBuilder;
+import DataMapper.TestCaseRowIndexes;
 import DataMapper.TestDataVariables;
 import DataMapper.ExcelReader;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.testng.annotations.*;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+
+import static DataMapper.ConfigBuilder.TestCasesPath;
+import static DataMapper.ConfigBuilder.configReader;
+import static DataMapper.TestCaseRowIndexes.testCaseRows;
 
 
 public class automationEngine {
     static Sheet testcasesData;
     static KeywordActionsReflection reflection = new KeywordActionsReflection();
     static TestDataVariables testdata = new TestDataVariables();
-    public static keywordActions actions1 = new keywordActions();
-
-
-
 
     @Test
     public static void Engine() throws IOException, InvalidFormatException, InterruptedException, InvocationTargetException, IllegalAccessException, NoSuchMethodException {
-        ConfigBuilder configbuilder = new ConfigBuilder();
-        configbuilder.configReader();
-        keywordActions.createDriverInstance(ConfigBuilder.Browser, ConfigBuilder.BrowserDriverPath);
+        //ConfigBuilder configbuilder = new ConfigBuilder();
+        configReader();
         ExcelReader reader = new ExcelReader();
+        testcasesData = reader.TestCaseReader(TestCasesPath);
+        TestCaseRowIndexes testcases = new TestCaseRowIndexes();
+        testcases.buildTestCasesList(ConfigBuilder.TestCases, testcasesData);
 
-        testcasesData = reader.TestCaseReader(ConfigBuilder.TestCasesPath);
         System.out.println(testcasesData.getLastRowNum());
+        for (String key:testCaseRows.keySet()) {
+            keywordActions.createDriverInstance(ConfigBuilder.Browser, ConfigBuilder.BrowserDriverPath);
+            //System.out.println(key );
+            for(int i=0;i<testCaseRows.get(key).size();i++) {
+                System.out.println(testCaseRows.get(key).get(i));
+                 testdata.setVariables(testcasesData, Integer.parseInt(testCaseRows.get(key).get(i).toString()));
+                reflection.getMethods();
+                reflection.performAction(TestDataVariables.Identifier, TestDataVariables.InputLocator, TestDataVariables.InputData, TestDataVariables.Action);
+            }
+        }
 
+
+/*
         for (int id = 0; id < ConfigBuilder.numOfTestCases; id++) {
 
             testcasesData.getRow(id).getRowNum();
@@ -41,8 +54,6 @@ public class automationEngine {
                 testdata.setVariables(testcasesData, i);
                 reflection.getMethods();
                 reflection.performAction(TestDataVariables.Identifier, TestDataVariables.InputLocator, TestDataVariables.InputData, TestDataVariables.Action);
-
-            }
-        }
+*/
     }
 }
